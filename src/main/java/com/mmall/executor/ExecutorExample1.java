c9b5792c4e06ae2f15b4208ccb8433b8d2dc07fa
cwl-0514 <980697938@qq.com>
2018-09-08 16:14:25 +0800

    线程池练习

---
+++ src/main/java/com/mmall/executor/ExecutorExample1.java
@@ -0,0 +1,17 @@
+package com.mmall.executor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by cwl on 2018/9/8.
+ */
+public class ExecutorExample1 {
+    public static void main(String[] args){
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //要学习线程创建的各个接口的意义和里面参数的意义
+
+
+    }
+}
